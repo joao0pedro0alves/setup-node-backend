ebc3b0558fd124e2b29a3a5fa63fcc2511b6a71f
João Alves <joao.alves@ecosis.com.br>
2023-02-20 08:39:47 -0300

    feat: node api with  and tests

---
+++ src/repositories/in-memory/in-memory-appointment-repository.ts
@@ -0,0 +1,28 @@
+import { areIntervalsOverlapping } from "date-fns";
+
+import { Appointment } from "../../models/appointment";
+import { AppointmentsRepository } from "../appointment-repository";
+
+export class InMemoryAppointmentsRepository implements AppointmentsRepository {
+    public items: Appointment[] = []
+
+    async create(appointment: Appointment): Promise<void> {
+        this.items.push(appointment)
+    }
+
+    async findOverlapingAppointment(startsAt: Date, endsAt: Date): Promise<Appointment | null> {
+            const overlappingAppointment = this.items.find(appointment => {
+                return areIntervalsOverlapping(
+                    { start: startsAt, end: endsAt },
+                    { start: appointment.startsAt, end: appointment.endsAt },
+                    { inclusive: true }
+                )
+            })
+
+            if (!overlappingAppointment) {
+                return null
+            }
+
+            return overlappingAppointment
+    }
+}
---
+++ src/use-cases/create-appointment.ts
@@ -0,0 +1,39 @@
+import { Appointment } from '../models/appointment';
+import { AppointmentsRepository } from '../repositories/appointment-repository';
+
+interface CreateAppointmentRequest {
+    customer: string;
+    startsAt: Date;
+    endsAt: Date;
+}
+
+type CreateAppointmentResponse = Appointment
+
+export class CreateAppointment {
+    constructor(
+        private appointmentsRepository: AppointmentsRepository
+    ) {}
+
+    async execute({
+        customer,
+        endsAt,
+        startsAt,
+    }: CreateAppointmentRequest): Promise<CreateAppointmentResponse> {
+
+        const overlappingAppointment = await this.appointmentsRepository.findOverlapingAppointment(startsAt, endsAt)
+
+        if (overlappingAppointment) {
+            throw new Error('Another appointment overlaps this appointment dates')
+        }
+
+        const appointment = new Appointment({
+            customer,
+            endsAt,
+            startsAt,
+        })
+
+        await this.appointmentsRepository.create(appointment)
+
+        return appointment
+    }
+}
