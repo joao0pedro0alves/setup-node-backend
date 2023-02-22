331727af7dfdda237d2adcd382e9efec52b2f20c
João Alves <joao.alves@ecosis.com.br>
2023-02-22 14:31:14 -0300

    chore: setup prettier and eslint

--- src/repositories/appointment-repository.ts
+++ src/repositories/appointment-repository.ts
@@ -1,6 +1,9 @@
 import { Appointment } from '../models/appointment'
 
 export interface AppointmentsRepository {
-    create(appointment: Appointment): Promise<void>;
-    findOverlapingAppointment(startsAt: Date, endsAt: Date): Promise<Appointment | null>
+    create(appointment: Appointment): Promise<void>
+    findOverlapingAppointment(
+        startsAt: Date,
+        endsAt: Date
+    ): Promise<Appointment | null>
 }
