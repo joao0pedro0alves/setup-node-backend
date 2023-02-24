4c6e4047c847d16d535cb4f4960437020f22750a
João Alves <joao.alves@ecosis.com.br>
2023-02-24 08:52:47 -0300

    chore: remove example files

--- src/infra/database/repositories/appointment-repository.ts
+++
@@ -1,9 +0,0 @@
-import { Appointment } from '../../../domain/entities/appointment';
-
-export interface AppointmentsRepository {
-    create(appointment: Appointment): Promise<void>;
-    findOverlapingAppointment(
-        startsAt: Date,
-        endsAt: Date
-    ): Promise<Appointment | null>;
-}
